90a1774cb3fb56bac3374c785ad7e75a657fc4d5
microman92 <90110834+microman92@users.noreply.github.com>
2023-04-20 16:28:29 +0500

    added static and templates

---
+++ pages/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+"""
+{% url 'main:home' %}
+{% url 'users:home' %}
+"""
+app_name = "main"
+
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path("shop/", views.shop_view, name="shop"),
+]
